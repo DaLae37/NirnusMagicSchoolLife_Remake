2ec1c0794690b3f4782f58d32aa0c2d98372bd51
DaLae37 <dalae37@gmail.com>
2023-04-11 13:24:24 +0900

    Temporary Backup

--- NirnusMagicSchoolLife/DL-Engine/MainScene.cpp
+++ NirnusMagicSchoolLife/DL-Engine/MainScene.cpp
@@ -1,9 +1,11 @@
 #include "stdafx.h"
 #include "MainScene.h"
+#include "GameScene.h"
 
 MainScene::MainScene() {
 	icon = new Sprite(L"Resources/Images/MainScene/mainScene.jpg");
 	icon->setPos(0, 0);
+	icon->setRotationCenter(icon->getWidth() / 2, icon->getHeight() / 2);
 	AddObject(icon);
 
 	soundManager->LoadAudioFromFile(L"Resources/Sounds/BackgroundSound.wav");
@@ -19,4 +21,9 @@ void MainScene::Render() {
 
 void MainScene::Update(float dTime) {
 	Scene::Update(dTime);
+	icon->setRotation(180);
+
+	if (inputManager->GetKeyState(VK_SPACE) == KEY_DOWN) {
+		sceneManager->ChangeScene(new GameScene());
+	}
 }
